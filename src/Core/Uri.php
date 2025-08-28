23ea4762bf0c362cc9b27f14e6eaa1d32ec1f8bc
Taka Oyama <toyama4649@hotmail.com>
2025-08-28 22:38:11 +0900

    add more tests

--- src/Core/Uri.php
+++ src/Core/Uri.php
@@ -3,74 +3,93 @@
 namespace Kirameki\Core;
 
 use function assert;
+use function dump;
+use function implode;
 use function parse_url;
 
 class Uri
 {
     /**
-     * @param string $url
+     * @param string $uri
      * @return static
      */
-    public static function parse(string $url): self
+    public static function parse(string $uri): self
     {
-        $parsed = parse_url($url);
-        assert($parsed !== false, 'Invalid URL');
-        return new static(...$parsed);
+        $parsed = parse_url($uri);
+        assert($parsed !== false, 'Invalid URI: ' . $uri);
+        return new static(
+            scheme: $parsed['scheme'] ?? 'http',
+            hostname: $parsed['host'] ?? 'localhost',
+            port: $parsed['port'] ?? null,
+            path: $parsed['path'] ?? '',
+            query: $parsed['query'] ?? '',
+            fragment: $parsed['fragment'] ?? '',
+            username: $parsed['user'] ?? '',
+            password: $parsed['pass'] ?? '',
+        );
+    }
+
+    /**
+     * @var string
+     */
+    public string $protocol {
+        get => "{$this->scheme}:";
+    }
+
+    /**
+     * @return string
+     */
+    public string $userinfo {
+        get => $this->password !== '' ? "{$this->username}:{$this->password}" : $this->username;
+    }
+
+    /**
+     * @return string
+     */
+    public string $host {
+        get => $this->hostname . ($this->port !== null ? ":{$this->port}" : '');
+    }
+
+    /**
+     * @return string
+     */
+    public string $authority {
+        get => $this->getUserInfoComponent() . $this->host;
+    }
+
+    /**
+     * @return string
+     */
+    public string $origin {
+        get => $this->protocol . '//' . $this->host;
     }
 
     /**
      * @param string $scheme
-     * @param string $host
+     * @param string $hostname
      * @param int $port
      * @param string $path
      * @param string $query
      * @param string $fragment
-     * @param string $user
-     * @param string $pass
+     * @param string $username
+     * @param string $password
      */
     public function __construct(
         public readonly string $scheme,
-        public readonly string $host,
+        public readonly string $hostname,
         public readonly ?int $port = null,
         public readonly string $path = '',
         public readonly string $query = '',
         public readonly string $fragment = '',
-        public readonly string $user = '',
-        public readonly string $pass = '',
+        public readonly string $username = '',
+        public readonly string $password = '',
     ) {
-    }
-
-    /**
-     * @return string
-     */
-    public function userinfo(): string
-    {
-        $userinfo = $this->user;
-        if ($userinfo !== '' && $this->pass !== '') {
-            $userinfo.= ':'.$this->pass;
-        }
-        return $userinfo;
-    }
-
-    /**
-     * @return string
-     */
-    public function authority(): string
-    {
-        $host = $this->host;
-        $port = $this->port;
-        $authority = $userinfo = $this->userinfo();
-        $authority .= ($userinfo !== '') ? '@' . $host : $host;
-        if ($port !== null && $host !== '') {
-            $authority .= ':' . $port;
-        }
-        return $authority;
     }
 
     /**
      * @return array<array<string, mixed>|string>
      */
-    public function queryParameters(): array
+    public function parseQuery(): array
     {
         $result = [];
         parse_str($this->query, $result);
@@ -78,20 +97,35 @@ class Uri
     }
 
     /**
-     * @return string|null
+     * @return string
      */
-    public function pathAndQuery(): ?string
+    public function getProtocolComponent(): string
     {
-        $str = null;
-        $path = $this->path;
-        if ($path !== '') {
-            $str.= $path;
-        }
-        $query = $this->query;
-        if ($query !== '') {
-            $str.= '?'.$query;
-        }
-        return $str;
+        return "{$this->protocol}//";
+    }
+
+    /**
+     * @return string
+     */
+    public function getUserInfoComponent(): string
+    {
+        return $this->userinfo !== '' ? "{$this->userinfo}@" : '';
+    }
+
+    /**
+     * @return string
+     */
+    public function getQueryComponent(): string
+    {
+        return $this->query !== '' ? "?{$this->query}" : '';
+    }
+
+    /**
+     * @return string
+     */
+    public function getFragmentComponent(): string
+    {
+        return $this->fragment !== '' ? "#{$this->fragment}" : '';
     }
 
     /**
@@ -107,20 +141,13 @@ class Uri
      */
     public function __toString(): string
     {
-        $str = $this->scheme;
-        $str.= '://'.$this->authority();
-        $path = $this->path;
-        if ($path !== '') {
-            $str.= $path;
-        }
-        $query = $this->query;
-        if ($query !== '') {
-            $str.= '?'.$query;
-        }
-        $fragment = $this->fragment;
-        if ($fragment !== '') {
-            $str.= '#'.$fragment;
-        }
-        return $str;
+        return implode('', [
+            $this->getProtocolComponent(),
+            $this->getUserInfoComponent(),
+            $this->host,
+            $this->path,
+            $this->getQueryComponent(),
+            $this->getFragmentComponent(),
+        ]);
     }
 }
